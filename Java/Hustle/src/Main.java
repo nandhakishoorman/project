f2970e52575f7873414764c334d5b58485528933
Nandha kishoor <nandhakishoorman@gmail.com>
2025-08-15 14:05:59 +0530

    Remove unwanted

---
+++ Java/Hustle/src/Main.java
@@ -0,0 +1,15 @@
+// c = 2(pi)(r)
+// a = pi(r * r)
+// v = 4/3(pi * r * r * r)
+public class Main{
+    public static void main(String[] args){
+        double c, r, a, v;
+        r=10;
+        c = 2*Math.PI*r;
+        System.out.printf("%.1f\n",c);
+        a = Math.PI * (Math.pow(r, 2));
+        System.out.printf("%.1f\n",a);
+        v = 4/3.0 * (Math.PI * Math.pow(r, 3));
+        System.out.printf("%.1f\n",v);
+    }
+}
