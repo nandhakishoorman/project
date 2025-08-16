f2057109c562356dd3aff168886e8e32fa1df2f1
Nandha kishoor <nandhakishoorman@gmail.com>
2025-08-16 19:38:36 +0530

    Feat: Temp Convertion Program

--- Java/Hustle/src/Main.java
+++ Java/Hustle/src/Main.java
@@ -1,13 +1,19 @@
-// Tertiary operator
+// (0°C × 9/5) + 32 = 32°F
+//(32°F − 32) × 5/9 = 0°C
 
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter ur marks:- ");
-        int score = scanner.nextInt();
-        String pass_or_fail = (score >= 60) ? "PASS" : "FAIL";
-        System.out.println(pass_or_fail);
+        String current_type;
+        double temp;
+        double convertion;
+        System.out.println("From 'c' to 'f' or 'f' to 'c', what the current type: ");
+        current_type = scanner.nextLine();
+        System.out.println("Enter the temp: ");
+        temp = scanner.nextDouble();
+        convertion = (current_type.equalsIgnoreCase("C")) ? (temp* 9.0/5.0) + 32 : (temp - 32) * 5.0/9.0;
+        System.out.println(convertion);
     }
 }
