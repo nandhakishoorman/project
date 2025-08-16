3c4755309d165750df367f13b1448adc07d382db
Nandha kishoor <nandhakishoorman@gmail.com>
2025-08-16 19:19:27 +0530

    Feature: A substring program:

--- Java/Hustle/src/Main.java
+++ Java/Hustle/src/Main.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        double principal = 1000;
-        double interest_rate = 0.06;  // 6% as decimal
-        double no_of_time_compounded = 1; // Compounded once per year
-        double time = 5;  // 5 years
-
-        double amount = principal * Math.pow((1 + interest_rate / no_of_time_compounded),
-                no_of_time_compounded * time);
-
-        System.out.println("Final Amount after " + time + " years = " + amount);
+public class Main{
+    public static void main(String[] args){
+        String email;
+        int len;
+        String the_name;
+        email = "nandhakishoorman@gmail.com";
+        len = email.length();
+        the_name = email.substring(0, 16);
+        System.out.println(the_name);
     }
 }
