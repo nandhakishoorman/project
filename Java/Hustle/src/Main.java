cb4cf0c21eccad3548b1a7c7d269e4e10189ea63
Nandha kishoor <nandhakishoorman@gmail.com>
2025-08-19 21:23:11 +0530

    A simple infinite loop(while method)

--- Java/Hustle/src/Main.java
+++ Java/Hustle/src/Main.java
@@ -1,20 +1,9 @@
-import java.util.Scanner;
-
-public class Main {
+public class Main{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the day: ");
-        String day = scanner.nextLine();
-
-        switch(day){
-            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ->
-                System.out.println("Bruh");
-            case "Saturday", "Sunday" ->
-                System.out.println("Enjoy! Bruh!");
-            default ->{
-                System.out.println("That's not in my boss head when he was coding!");
-                System.out.println("Jesus is the way, the truth and the life.");
-            }
+        System.out.println("Hello");
+        int i = 10;
+        while (i!=10-9){
+            System.out.println("You are in a infinite loop!");
         }
     }
 }
