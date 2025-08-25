687c2ed4789c5f1bf3a136971b7cd2f29e55bc9d
Nandha kishoor <nandhakishoorman@gmail.com>
2025-08-25 21:15:04 +0530

    Feat: Class and construction.

---
+++ Java/Hustle/src/Student.java
@@ -0,0 +1,16 @@
+public class Student{
+    int time_of_living;
+    String name;
+    double gpa;
+    int year;
+    int month;
+
+    //Construction
+    Student (int age, String name,double gpa,int year,int month){
+        this.time_of_living= age;
+        this.name= name;
+        this.gpa = gpa;
+        this.year= year;
+        this.month = month;
+    }
+}
